e4fc231fc8b10513f054fbcbd0b22466e393facd
shubhamaryal <aryalshubham62@gmail.com>
2025-11-20 19:09:01 +0545

    halftone effect

--- 9. Halftone Shading/src/shaders/halftone/fragment.glsl
+++ 9. Halftone Shading/src/shaders/halftone/fragment.glsl
@@ -33,13 +33,28 @@ void main()
     color *= light;
 
     // Halftone 
+    // float repetitions = 50.0;
+    float repetitions = 10.0;
+
     // vec2 uv = gl_FragCoord.xy;
     // vec2 uv = gl_FragCoord.xy / 1000.0;
-    vec2 uv = gl_FragCoord.xy / uResolution;
+    // vec2 uv = gl_FragCoord.xy / uResolution;
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    // uv *= 50.0;
+    // uv *= 10.0;
+    uv *= repetitions;
+    uv = mod(uv, 1.0);
+
+    float point = distance(uv, vec2(0.5));
+    // point = step(0.5, point);
+    point = 1.0 - step(0.5, point);
+    // point = 1.0 - step(0.5 * 0.3, point);
+    // point = step(point, 0.5);
 
     // Final color
     // gl_FragColor = vec4(color, 1.0);
-    gl_FragColor = vec4(uv, 1.0, 1.0);
+    // gl_FragColor = vec4(uv, 1.0, 1.0);
+    gl_FragColor = vec4(vec3(point), 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
