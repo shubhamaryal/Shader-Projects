5e749a80621dc123a69291940698907298891384
shubhamaryal <aryalshubham62@gmail.com>
2026-01-27 21:13:39 +0545

    sliced model

--- 15. Sliced Model/src/script.js
+++ 15. Sliced Model/src/script.js
@@ -47,6 +47,14 @@ rgbeLoader.load('./aerodynamics_workshop.hdr', (environmentMap) =>
 /**
  * Sliced model
  */
+const uniforms = {
+    uSliceStart: new THREE.Uniform(1.75),
+    uSliceArc: new THREE.Uniform(1.25),
+}
+
+gui.add(uniforms.uSliceStart, 'value', - Math.PI, Math.PI, 0.001).name('uSliceStart')
+gui.add(uniforms.uSliceArc, 'value', 0, Math.PI * 2, 0.001).name('uSliceArc')
+
 // // Geometry
 // const geometry = new THREE.IcosahedronGeometry(2.5, 5)
 
@@ -64,6 +72,7 @@ const slicedMaterial = new CustomShaderMaterial({
     baseMaterial: THREE.MeshStandardMaterial,
     vertexShader: slicedVertexShader,
     fragmentShader: slicedFragmentShader,
+    uniforms: uniforms,
 
     // MeshStandardMaterial
     metalness: 0.5,
--- 15. Sliced Model/src/shaders/sliced/fragment.glsl
+++ 15. Sliced Model/src/shaders/sliced/fragment.glsl
@@ -1,13 +1,19 @@
+uniform float uSliceStart;
+uniform float uSliceArc;
+
 varying vec3 vPosition;
 
 void main() {
-    float uSliceStart = 1.0;
-    float uSliceArc = 1.5;
+    // float uSliceStart = 1.0;
+    // float uSliceArc = 1.5;
 
     float angle = atan(vPosition.y, vPosition.x);
 
+    if(angle > uSliceStart && angle < uSliceArc + uSliceArc)
+        discard;
+
     // csm_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     // csm_FragColor = vec4(vPosition, 1.0);
-    // csm_FragColor = vec4(vec3(angle), 1.0);
-    csm_FragColor = vec4(vec3(abs(angle)), 1.0);
+    csm_FragColor = vec4(vec3(angle), 1.0);
+    // csm_FragColor = vec4(vec3(abs(angle)), 1.0);
 }
